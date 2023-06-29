9daf10f2442d42a132e611b6828e57e4ac8a86f0
unknown <318277446@qq.com>
2023-06-29 16:55:27 +0800

    done

---
+++ WebserviceDemo/WebService1.asmx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebserviceDemo
+{
+    /// <summary>
+    /// WebService1 的摘要说明
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消注释以下行。 
+     [System.Web.Script.Services.ScriptService]
+     
+
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public string GetAge(string id)
+        {
+            return "ID为:" + id + "的年龄为:" + new Random().Next(10, 41);
+        }
+
+        /// <summary>
+        /// 用于前端跨越调用（jsonp）
+        /// </summary>
+        /// <param name="name"></param>
+        [WebMethod]
+        public void HelloWorld1(string name)
+        {
+            HttpContext.Current.Response.ContentType = "application/json;charset=utf-8";
+            string jsonCallBackFunName = string.Empty;
+            jsonCallBackFunName = HttpContext.Current.Request.Params["jsoncallback"].ToString();
+            HttpContext.Current.Response.Write(jsonCallBackFunName + "({ \"Result\": \"Helloword2" + name + "\" })");
+        }
+
+        /// <summary>
+        /// 用于前端跨域调用（在system.webServer下增加可跨域访问配置）
+        /// </summary>
+        /// <param name="name"></param>
+        [WebMethod]
+        public string HelloWorld2(string name)
+        {
+            return "{data:你好：" + name + "}";
+        }
+    }
+}
---
+++ net  调用 webService（服务引用和动态代理）/Program.cs
@@ -0,0 +1,38 @@
+﻿using net__调用_webService_服务引用和动态代理_.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net__调用_webService_服务引用和动态代理_
+{
+    class Program
+    {
+
+        #region 用于测试调用webservice（服务引用方式）
+        public static void Execute1()
+        {
+            ServiceReference1.WebService1SoapClient testServer = new ServiceReference1.WebService1SoapClient();
+            string str1 = testServer.HelloWorld();
+            string str2 = testServer.GetAge("b101");
+            Console.Write(str1 + "," + str2);
+        }
+        #endregion
+
+        #region 用于测试调用webservice（动态代理方式）
+        public static void Execute2()
+        {
+            string str1 = WSHelper.InvokeWebService(url: "http://localhost:4116/WebService1.asmx", classname: "WebService1", methodname: "HelloWorld").ToString();
+            Console.Write(str1);
+            string str2 = WSHelper.InvokeWebService(url: "http://localhost:4116/WebService1.asmx", classname: "WebService1", methodname: "GetAge", args: new string[] { "b101" }).ToString();
+            Console.Write(str2);
+        }
+        #endregion
+        static void Main(string[] args)
+        {
+            Execute1();
+            Execute2();
+        }
+    }
+}
